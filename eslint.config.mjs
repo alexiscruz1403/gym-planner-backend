c58ed2c491fe7cd461e55179852f97c44f35edf3
Alexis Cruz <jr.alexis.cruz.2003@gmail.com>
2026-03-24 18:54:06 -0300

    chore: initial NestJS project setup with ESLint, Prettier and Husky

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+// eslint.config.mjs
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    ignores: ['.eslintrc.js', 'node_modules'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+      },
+      globals: {
+        node: true,
+        jest: true,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      'no-console': 'warn',
+      'prettier/prettier': 'error',
+    },
+  },
+];
