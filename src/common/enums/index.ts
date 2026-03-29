19a886bdd7f0f37777e1395b51b333fa1b1c8b7c
Alexis Cruz <jr.alexis.cruz.2003@gmail.com>
2026-03-29 11:48:10 -0300

    feat(session): add WorkoutSession schema and SessionStatus enum

--- src/common/enums/index.ts
+++ src/common/enums/index.ts
@@ -1,3 +1,4 @@
 export * from './muscle-group.enum';
 export * from './load-type.enum';
 export * from './day-of-week.enum';
+export * from './session-status.enum';
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,3 +2,4 @@ export * from './user.schema';
 export * from './refresh-token.schema';
 export * from './exercise.schema';
 export * from './workout-plan.schema';
+export * from './workout-session.schema';
