991eaba9e69b135c1ba81e8382eaaa58461a7403
Alexis Cruz <jr.alexis.cruz.2003@gmail.com>
2026-04-04 23:31:34 -0300

    fix(prod): fix frontend cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true, // Reflects the request origin, allowing requests from any domain
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
